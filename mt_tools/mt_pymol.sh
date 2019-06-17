6712e73f0c5f9f834bf4427ed6e2146c7bab10be
Lorenzo Gaifas <brisvag@gmail.com>
2019-06-17 13:25:33 +0200

    requires python3

--- mt_tools/mt_pymol.sh
+++ mt_tools/mt_pymol.sh
@@ -5,7 +5,7 @@
 dir_path="`dirname \"$0\"`"
 
 if command -v python3 > /dev/null 2>&1; then
-    $dir_path/mt_pymol.py $@
+    python3 $dir_path/mt_pymol.py $@
 else
     echo "You don't seem to have python3 installed!"
     echo "You need python3 to run mt_pymol."

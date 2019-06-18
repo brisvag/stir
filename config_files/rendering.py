8bc0c5c83410a592ad6087ea856bd9b6a6f18f09
Lorenzo Gaifas <brisvag@gmail.com>
2019-06-18 15:34:02 +0200

    changed some rendering settings

--- config_files/rendering.py
+++ config_files/rendering.py
@@ -23,23 +23,23 @@ cmd.set('ray_shadow', 'on')
 cmd.set('light_count', 4)
 
 # TODO: hard to optimize these settings. Need more testing
-cmd.set('ambient', 0.25000)
-cmd.set('direct', 0.25000)
-cmd.set('reflect', 0.80000)
-cmd.set('shininess', 10.00000)
-cmd.set('specular', 'off')
+cmd.set('ambient', 0.1)
+cmd.set('direct', 0.1)
+cmd.set('reflect', 1.3)
+cmd.set('shininess', 10.0)
+cmd.set('specular', 0.3)
 cmd.set('spec_count', 1)
-cmd.set('power', 1.00000)
-cmd.set('specular_intensity', 0.10000)
-cmd.set('spec_direct', 0.10000)
+cmd.set('power', 1.0)
+cmd.set('specular_intensity', 0.1)
+cmd.set('spec_direct', 0.2)
 
 cmd.set('ray_shadow_decay_factor', 0.10000)
 cmd.set('ray_shadow_decay_range', 2.00000)
 
-# ambient occlusion is better than shadows
-cmd.set('ambient_occlusion_mode', 2)
-cmd.set('ambient_occlusion_scale', 13)
-cmd.set('ambient_occlusion_smooth', 20)
+# TODO: ambient occlusion is better than shadows when rendering surfaces
+#cmd.set('ambient_occlusion_mode', 2)
+#cmd.set('ambient_occlusion_scale', 13)
+#cmd.set('ambient_occlusion_smooth', 20)
 
 cmd.set('surface_quality', 2)
 cmd.set('sphere_quality', 4)

0e954a5629fc41fa0c1812ae25381422c40a42f0
Lorenzo Gaifas <brisvag@gmail.com>
2019-06-21 16:36:23 +0200

    mt_nice works as intended

--- mt_tools/mt_nice.py
+++ mt_tools/mt_nice.py
@@ -2,42 +2,14 @@ from pymol import cmd, stored
 import random
 
 
-selectors = {
-    'prot': 'polymer.protein',
-    'BB': 'polymer.protein and name BB',
-    'SC': 'polymer.protein and name SC*',
-    'solv': 'resname W or resname WN or resname ION',
-    'ions': 'resname ION',
-    'lip': 'organic',
-    'nucl': 'polymer.nucleic'
-}
-
-
-def mt_sele(delete=None):
+def nice_settings():
     """
-    easily select relevant groups of atoms
+    sets up a series of settings and stores in pymol.stored to be used at later points by mt_tools
 
-    Usage:
-
-    to create selections: mt_sele
-    to delete them: mt_sele delete
-
-    See also: mt_desele
-    """
-    if delete:
-        if delete == 'delete':
-            for sel in selectors:
-                cmd.delete(sel)
-        else:
-            print('Unknown option. Type `mt_sele delete` to delete all the mt selections')
-    else:
-        for sel, logic in selectors.items():
-            cmd.select(sel, logic)
-
-
-def nice_colors():
-    """
-    creates a set of nicely distinguishable colors and stores their pymol ids in stored.mt_colors
+    parts:
+    - a set of nicely distinguishable colors for mt_color
+    - a dictionary of selection expressions for mt_sele
+    - a dictionary of representation information for mt_nice
     """
     # colors are based on this SO answer:
     # https://graphicdesign.stackexchange.com/questions/3682/
@@ -77,10 +49,109 @@ def nice_colors():
         cmd.set_color(col_name, rgb)
         mt_colors[col_name] = rgb
 
+    # store color ids in a list to be used by mt_color
     stored.mt_colors = []
     for color in mt_colors:
         idx = cmd.get_color_index(color)
         stored.mt_colors.append(idx)
+
+    # selection expressions
+    stored.mt_selectors = {
+        'prot': 'polymer.protein',
+        'BB': 'polymer.protein and name BB',
+        'SC': 'polymer.protein and name SC*',
+        'solv': 'resn W or resn WN or resn ION',
+        'ions': 'resn ION',
+        'lip': 'organic',
+        'nucl': 'polymer.nucleic'
+    }
+
+    # settings to be used by mt_nice. Values are lists with a function as first element and
+    # its arguments following
+    stored.mt_nice_set = {
+        'rainbow': {
+            'prot': {
+                'color_method': None,
+                'style': None,
+            },
+            'BB': {
+                'color_method': [mt_color, 'chain'],
+                'style': [cmd.show_as, 'sticks'],
+            },
+            'SC': {
+                'color_method': [mt_color, 'resn'],
+                'style': [cmd.show_as, 'sticks'],
+            },
+            'solv': {
+                'color_method': [cmd.color, 'blue'],
+                'style': [cmd.hide, 'everything'],
+            },
+            'ions': {
+                'color_method': [mt_color, 'resn'],
+                'style': [cmd.show_as, 'spheres'],
+            },
+            'lips': {
+                'color_method': [mt_color, 'resi'],
+                'style': [cmd.show_as, 'sticks'],
+            },
+            'nucl': {
+                'color_method': [mt_color, 'resn'],
+                'style': [cmd.show_as, 'sticks'],
+            },
+        },
+        'clean': {
+            'prot': {
+                'color_method': None,
+                'style': None,
+            },
+            'BB': {
+                'color_method': [mt_color, 'chain'],
+                'style': [cmd.show_as, 'sticks'],
+            },
+            'SC': {
+                'color_method': None,
+                'style': [cmd.hide, 'everything'],
+            },
+            'solv': {
+                'color_method': None,
+                'style': [cmd.hide, 'everything'],
+            },
+            'ions': {
+                'color_method': None,
+                'style': [cmd.hide, 'everything'],
+            },
+            'lip': {
+                'color_method': [mt_color, 'resn'],
+                'style': [cmd.show_as, 'sticks'],
+            },
+            'nucl': {
+                'color_method': [mt_color, 'resi'],
+                'style': [cmd.show_as, 'sticks'],
+            },
+        },
+    }
+
+
+def mt_sele(delete=None):
+    """
+    easily select relevant groups of atoms
+
+    Usage:
+
+    to create selections: mt_sele
+    to delete them: mt_sele delete
+
+    See also: mt_desele
+    """
+    if delete:
+        if delete == 'delete':
+            for sel in stored.mt_selectors:
+                cmd.delete(sel)
+        else:
+            print('Unknown option. Type `mt_sele delete` to delete all the mt selections')
+    else:
+        for sel, logic in stored.mt_selectors.items():
+            cmd.select(sel, logic)
 
 
 def mt_color(method, selection='all'):
@@ -93,55 +164,27 @@ def mt_color(method, selection='all'):
     cmd.recolor()
 
 
-def mt_nice(selection='all'):
-    # TODO: add multiple choice option, e.g: pretty|bilayer|vdw?
+def mt_nice(main_sele='all', style='clean'):
     mt_sele()
 
     cmd.set('stick_radius', 0.7)
 
-    nice_settings = {
-        'prot': {
-            'color_method': None,
-            'style': 'stick'
-        },
-        'BB': {
-            'color_method': 'chain',
-            'style': 'stick'
-        },
-        'SC': {
-            'color_method': 'resi',
-            'style': 'stick'
-        },
-        'solv': {
-            'color_method': None,
-            'style': 'sphere'
-        },
-        'ions': {
-            'color_method': 'name',
-            'style': 'sphere'
-        },
-        'lip': {
-            'color_method': 'resi',
-            'style': 'stick'
-        },
-        'nucl': {
-            'color_method': 'chain',
-            'style': 'stick'
-        },
-    }
-
     # TODO: a nicer way to do this?
-    cmd.color('blue', f'{selection} and solv')
+    cmd.color('blue', f'{main_sele} and solv')
 
-    for sel, data in nice_settings.items():
-        if data['color_method']:
-            mt_color(data['color_method'], f'{selection} and {sel}')
-        if data['style']:
-            cmd.show_as(data['style'], f'{selection} and {sel}')
+    settings = stored.mt_nice_set[style]
+    for sele, commands in settings.items():
+        for command in commands.values():
+            if command:
+                # run function with its arguments. All functions must have `selection` as
+                # valid argument for this to work!
+                command[0](*command[1:], selection=f'{main_sele} and {sele}')
+            else:
+                continue
 
 
 def load():
-    nice_colors()
+    nice_settings()
     cmd.extend('mt_sele', mt_sele)
     cmd.extend('mt_color', mt_color)
     cmd.extend('mt_nice', mt_nice)

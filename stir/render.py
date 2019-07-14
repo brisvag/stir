c0f957b29a7af659e489b9cb2464dabdfe1cb27a
Lorenzo Gaifas <brisvag@gmail.com>
2019-07-14 16:49:11 +0200

    added single frame rendering

--- stir/render.py
+++ stir/render.py
@@ -6,26 +6,27 @@ from pymol import cmd, movie
 from . import config
 
 
-def cheese(movie_type, savefile=None, duration=5, mode='ray', width=1920, height=1080):
+def cheese(render_type, savefile=None, duration=5, mode='ray', width=1920, height=1080):
     """
 DESCRIPTION
 
-    loads nice settings for rendering and records a movie of the trajectory.
+    loads nice settings for rendering and  take a picture or records a movie of the trajectory.
     Before running, position the camera to have a good point of view.
 
 USAGE
 
-    cheese movie_type [, savefile [, duration [, mode [, width [, height]]]]]
+    cheese render_type [, savefile [, duration [, mode [, width [, height]]]]]
 
 ARGUMENTS
 
-    movie_type = one of:
-        - traj: records the whole trajectory
-        - bullettime: a whole revolution around the z axis
+    render_type = one of:
+        - snap: take a snapshot of the current view
+        - traj: record the whole trajectory
+        - bullettime: make a whole revolution around the z axis
     savefile = file name to use to save the movie, without extension. If not given,
                the movie won't be save but only shown (default=None)
     duration = duration in seconds of the final movie (default=5)
-    ray = set to 'draw' to disable ray tracing (default='ray')
+    mode = set to 'draw' to disable ray tracing (default='ray')
     width = width of the movie in pixel (default=1920)
     height = height of the movie in pixel (default=1080)
     """
@@ -41,14 +42,19 @@ ARGUMENTS
     config.rendering()
     cmd.sync()
 
-    # initialize movie frames
-    cmd.mdelete()
-    cmd.mview('reset')
-    frames = duration*30
-    cmd.mset(f'1x{frames}')
+    def init_movie():
+        cmd.mdelete()
+        cmd.mview('reset')
+        frames = duration*30
+        cmd.mset(f'1x{frames}')
+        return frames
 
+    # if requested, take a picture
+    if render_type == 'snap':
+        cmd.ray(width=width, height=height)
     # make a still movie of the whole trajectory
-    if movie_type == 'traj':
+    elif render_type == 'traj':
+        frames = init_movie()
         cmd.mview('store', 1, state=1)
         cmd.sync()
         states = cmd.count_states()
@@ -57,7 +63,8 @@ ARGUMENTS
         cmd.mview('reinterpolate')
         cmd.sync()
     # make a movie rotating 360 around the z axis
-    elif movie_type == 'bullettime':
+    elif render_type == 'bullettime':
+        frames = init_movie()
         cmd.mview('store', 1)
         cmd.mview('store', frames)
         # TODO: moving objects seems a bit hacky. Any better solution?
@@ -78,7 +85,7 @@ ARGUMENTS
         cmd.mview('reinterpolate')
         cmd.sync()
     else:
-        raise ValueError(f'{movie_type} is not a valid argument. See `help cheese`.')
+        raise ValueError(f'{render_type} is not a valid argument. See `help cheese`.')
 
     # save if required, otherwise just play it
     if savefile:
